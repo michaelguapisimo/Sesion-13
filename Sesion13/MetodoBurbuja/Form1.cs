bd0390309df065ccac06d632f998a552c1555c7b
michaelguapisimo <mramirezc@uamv.edu.ni>
2024-09-30 10:21:11 -0600

    ds

---
+++ Sesion13/MetodoBurbuja/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetodoBurbuja.models;
+
+namespace MetodoBurbuja
+{
+    public partial class Form1 : Form
+    {
+        int[] numbers = new int [10];
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = int.Parse(tbNumber.Text);
+                numbers[i++] = number;
+                ShowNumbers();
+                Clear();
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void ShowNumbers()
+        {
+            if (lbNumbers.Items.Count > 0) 
+            {
+                lbNumbers.Items.Clear();
+            }
+            foreach (int number in numbers) 
+            {
+                lbNumbers.Items.Add(number);
+            }
+        }
+        private void Clear()
+        {
+            tbNumber.Clear();
+            tbNumber.Focus();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            bubble bubble = new bubble();
+            bubble.BubbleSort(numbers);
+            ShowNumbers();
+        }
+    }
+}
---
+++ Sesion13/MetodoBurbuja/models/bubble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoBurbuja.models
+{
+    internal class bubble
+    {
+        public void BubbleSort(int[] array) 
+        {
+            try
+            {
+                for (int i = 0; i < array.Length; i++) { 
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[i] == array[j])
+                        {
+                            int temp = array[i];
+                            array[i] = array[j];
+                            array[j] = temp;
+
+                        }
+                    }                             
+                }    
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }   
+    }
+}
